43f4263032e38c28f1797ab8ccb1af85e41c9c88
Tristan Farneau <tfarneau@gmail.com>
2015-12-17 12:28:04 +0100

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,7 +24,7 @@ class UserController extends ApiController
 
     public function index()
     {
-        $user =  User::transform([JWTAuth::parseToken()->authenticate()]);
+        $user =  User::transform(JWTAuth::parseToken()->authenticate());
         return $this->respondSuccess('SUCCESS', $user);
     }
 
